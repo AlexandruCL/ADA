ca830e52504e8cff56339e5a12acd8c9009421fc
Alexandru CL <laur.cristea@student.upt.ro>
2025-03-01 01:26:36 +0200

    a

--- Week1/src/StackClient7.java
+++ Week1/src/StackClient7.java
@@ -1,4 +1,3 @@
-import javax.print.attribute.HashDocAttributeSet;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,12 +7,12 @@ import java.util.Stack;
 public class StackClient7 {
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
-        System.out.println("Enther the name of the html file");
+        System.out.println("Enther the path of the html file");
 
         String fileName = scanner.nextLine();
 
         try {
-            if (validateHTML(fileName)) {
+            if (validateFILE(fileName)) {
                 System.out.println("The HTML file is correct.");
             } else {
                 System.out.println("The HTML file is not correct.");
@@ -23,33 +22,33 @@ public class StackClient7 {
         }
     }
 
-    public static boolean validateHTML(String filename) throws IOException{
+    public static boolean validateFILE(String filename) throws IOException{
         BufferedReader reader = new BufferedReader(new FileReader(filename));
         Stack<String> stack = new Stack<>();
         String line;
 
         while ((line = reader.readLine()) != null) {
-            int pos = 0;
-            while((pos = line.indexOf("<",pos)) != -1){
-                int end = line.indexOf(">",pos);
+            int startIndex = 0;
+            while((startIndex = line.indexOf("<", startIndex)) != -1){ //Find the starting tag index to compute the tag
+                int endIndex = line.indexOf(">", startIndex);
 
-                if(end == -1){
+                if(endIndex == -1){
                     reader.close();
                     return false;
                 }
 
-                String tag = line.substring(pos + 1,end);
+                String tag = line.substring(startIndex + 1, endIndex);
                 if(!tag.startsWith("/")){
                     stack.push(tag);
                 } else {
-                    if(stack.isEmpty() || !stack.peek().equals(tag.substring(1))){
+                    if(stack.isEmpty() || !stack.peek().equals(tag.substring(1))){ //Remove the / character from the closing tag in order to check it
                         reader.close();
                         return false;
                     } else {
                         stack.pop();
                     }
                 }
-                pos = end + 1;
+                startIndex = endIndex + 1;
             }
         }
         reader.close();
