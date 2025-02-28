09d5fd13063b6ead91f2667885654c1071feff77
Alexandru CL <laur.cristea@student.upt.ro>
2025-03-01 01:19:58 +0200

    a

--- Week1/src/StackClient2.java
+++ Week1/src/StackClient2.java
@@ -17,8 +17,8 @@ public class StackClient2 {
             String[] parts = line.split(" ", 2);
 
                 int number = Integer.parseInt(parts[0]);
-                intStack.push(number);
-                stringStack.push(parts[1]);
+                intStack.push(number);                      //Assuming that the input is correct and there is not a string firstly
+                stringStack.push(parts[1]);                 //Could place them in a try catch to check if the input is correct
         }
 
         if(n == 0 || intStack.isEmpty() || stringStack.isEmpty()){
