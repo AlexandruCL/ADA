ab7cbb1170604dc76d408809234c51560e4bdb7e
Alexandru CL <laur.cristea@student.upt.ro>
2025-03-01 01:17:14 +0200

    a

--- Week1/src/StackClient2.java
+++ Week1/src/StackClient2.java
@@ -16,13 +16,9 @@ public class StackClient2 {
             String line = sc.nextLine();
             String[] parts = line.split(" ", 2);
 
-            try{
                 int number = Integer.parseInt(parts[0]);
                 intStack.push(number);
                 stringStack.push(parts[1]);
-            }catch(NumberFormatException e){
-                System.out.println("Invalid format for the line");
-            }
         }
 
         if(n == 0 || intStack.isEmpty() || stringStack.isEmpty()){
